6a0e84f98e82f0c5de8e22e0a409cf31df95f0af
Sergeev Dima <web.sergeev.dima@gmail.com>
2023-08-16 22:38:40 +0300

    add styled com-s

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -4,20 +4,30 @@ import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
 
 const TaskInput = (props) => {
-  const [inputText, setinputText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [isInputValid, setIsInputValid] = useState(true);
 
   const taskInputChangeHandler = (event) => {
-    setinputText(event.target.value);
+    if(event.target.value.trim().length > 0) {
+      setIsInputValid(true);
+    }
+    setInputText(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    if(inputText.trim().length === 0) {
+      setIsInputValid(false);
+      return;
+    }
+
     props.onAddTask(inputText);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isInputValid ? 'invalid' : ''}`}>
         <label>Задачи</label>
         <input type="text" onChange={taskInputChangeHandler} />
       </div>
