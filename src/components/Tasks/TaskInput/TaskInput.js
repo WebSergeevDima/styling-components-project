97e88a46ec905e3e1b3026de3035408ce5112e42
Sergeev Dima <web.sergeev.dima@gmail.com>
2023-08-20 11:16:15 +0300

    update for css module

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,37 +1,37 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./TaskInput.css";
-import styled from "styled-components";
+import style from "./TaskInput.module.css";
+//import styled from "styled-components";
 
-const FormControl = styled.div`
-  & {
-    margin: 0.5rem 0;
-  }
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => props.invalid ? 'red' : '#000'};
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => props.invalid ? 'red' : '#000000'};
-    background: ${props => props.invalid ? '#ffd3d3' : '#ffffff'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #c8e1e4;
-    border-color: #00358b;
-  }
-`;
+// const FormControl = styled.div`
+//   & {
+//     margin: 0.5rem 0;
+//   }
+//
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => props.invalid ? 'red' : '#000'};
+//   }
+//
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.invalid ? 'red' : '#000000'};
+//     background: ${props => props.invalid ? '#ffd3d3' : '#ffffff'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+//
+//   & input:focus {
+//     outline: none;
+//     background: #c8e1e4;
+//     border-color: #00358b;
+//   }
+// `;
 
 const TaskInput = (props) => {
   const [inputText, setInputText] = useState("");
@@ -57,10 +57,14 @@ const TaskInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isInputValid && 'invalid'}>
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
-      </FormControl>
+      {/*<FormControl invalid={!isInputValid && 'invalid'}>*/}
+      {/*  <label>Задачи</label>*/}
+      {/*  <input type="text" onChange={taskInputChangeHandler} />*/}
+      {/*</FormControl>*/}
+      <div className={`${style['form-control']} ${!isInputValid && style.invalid}`}>
+          <label>Задачи</label>
+          <input type="text" onChange={taskInputChangeHandler} />
+      </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,33 +1,36 @@
-import styled from "styled-components";
-
-const Button = styled.button`
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    color: white;
-    background: #00358b;
-    border-radius: 4px;
-    box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
-    cursor: pointer;
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #245fbd;
-    box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
-  }
-
-`;
+//import styled from "styled-components";
+import style from './Button.module.css';
 
 
-// const Button = (props) => {
-//   return (
-//     <button type={props.type} className="button">
-//       {props.children}
-//     </button>
-//   );
-// };
+//
+// const Button = styled.button`
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     color: white;
+//     background: #00358b;
+//     border-radius: 4px;
+//     box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
+//     cursor: pointer;
+//
+//   &:focus {
+//     outline: none;
+//   }
+//
+//   &:hover,
+//   &:active {
+//     background: #245fbd;
+//     box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
+//   }
+//
+// `;
+
+
+const Button = (props) => {
+  return (
+    <button type={props.type} className={style.button}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
